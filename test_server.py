639ac43039f745e97772abd07b582e6fca1cb299
Steven Adam <sna219@nyu.edu>
2017-04-04 21:56:10 -0400

    sending json in request

--- test_server.py
+++ test_server.py
@@ -262,14 +262,20 @@ class TestRecommendationServer(unittest.TestCase):
         self.assertTrue(server.conn != None)
 
     def test_is_valid_errors_valueerror(self):
-        new_recommendation = {'parent_product_id': 'a', 'priority': '3', 'related_product_id':'3', 'type': 'up-sell'}
-        (message, status) = server.is_valid(new_recommendation)
+        new_recommendation = {'parent_product_id': 'hello', 'priority': '3', 'related_product_id':'3', 'type': 'up-sell'}
+        data = json.dumps(new_recommendation)
+        (message, status) = server.is_valid(data)
         self.assertEqual(status, False)
-    def test_is_valid_errors_typeerror(self):
-        new_recommendation = {'parent_product_id': [1], 'priority': '3', 'related_product_id':'3', 'type': 'up-sell'}
-        (message, status) = server.is_valid(new_recommendation)
-        self.assertEqual(status, False)
+        print message
+        self.assertTrue('value' in message)
 
+    def test_is_valid_errors_typeerror(self):
+        new_recommendation = {'parent_product_id': 'hello', 'priority': '3', 'related_product_id':'3', 'type': 'up-sell'}
+        data = json.dumps(new_recommendation)
+        (message, status) = server.is_valid(data)
+        self.assertEqual(status, False)
+        print message
+        self.assertTrue('Data type error' in message)
         
 ######################################################################
 # Utility functions
