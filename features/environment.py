388d8413f0df3cb6069cf393e033b3d23f4b63c7
Cheng-Hsi Chao <chc631@nyu.edu>
2017-04-06 17:46:18 -0400

    Remove troublesome function from behave's environ.

--- features/environment.py
+++ features/environment.py
@@ -5,5 +5,4 @@ import server
 def before_all(context):
     context.app = server.app.test_client()
     server.initialize_mysql(test=True)
-    server.initialize_index()
     context.server = server
